00c003078bb8a5f41c6c4b0e7d961302e2645067
TomRingblom <t_ringblom@hotmail.com>
2022-12-14 22:00:27 +0100

    Add OpenIdConnect config now working. Add SignIn/SigOut endpoint in HomePageController.

--- Features/Home/HomePageController.cs
+++ Features/Home/HomePageController.cs
@@ -1,5 +1,8 @@
 ﻿using EPiServer.Web.Mvc;
 using Labb.Features.Shared;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Labb.Features.Home;
@@ -7,4 +10,21 @@ namespace Labb.Features.Home;
 public class HomePageController : PageController<HomePage>
 {
     public ActionResult Index(HomePage currentContent) => View("~/Features/Home/Index.cshtml", ContentViewModel.Create(currentContent));
+
+    public ActionResult SignIn()
+    {
+        var scheme = OpenIdConnectDefaults.AuthenticationScheme;
+        var redirectUrl = Url.ActionContext.HttpContext.Request.Scheme + "://" +
+                          Url.ActionContext.HttpContext.Request.Host;
+        return Challenge(new AuthenticationProperties
+        {
+            RedirectUri = redirectUrl
+        }, scheme);
+    }
+
+    public ActionResult SignOut()
+    {
+        var scheme = OpenIdConnectDefaults.AuthenticationScheme;
+        return SignOut(new AuthenticationProperties(), CookieAuthenticationDefaults.AuthenticationScheme, scheme);
+    }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,15 @@
 using EPiServer.Cms.Shell;
+using EPiServer.Cms.TinyMce;
+using EPiServer.Cms.UI.Admin;
 using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Cms.UI.VisitorGroups;
 using EPiServer.Scheduler;
 using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Mvc.Html;
 using EPiServer.Web.Routing;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
@@ -14,13 +20,15 @@ namespace Labb;
 public class Startup
 {
     private readonly IWebHostEnvironment _webHostingEnvironment;
+    private readonly IConfiguration _configuration;
 
-    public Startup(IWebHostEnvironment webHostingEnvironment)
-    {
-        _webHostingEnvironment = webHostingEnvironment;
-    }
+	public Startup(IWebHostEnvironment webHostingEnvironment, IConfiguration configuration)
+	{
+		_webHostingEnvironment = webHostingEnvironment;
+		_configuration = configuration;
+	}
 
-    public void ConfigureServices(IServiceCollection services)
+	public void ConfigureServices(IServiceCollection services)
     {
         if (_webHostingEnvironment.IsDevelopment())
         {
@@ -29,79 +37,124 @@ public class Startup
             services.Configure<SchedulerOptions>(options => options.Enabled = false);
         }
 
-        services
-            .AddCmsAspNetIdentity<ApplicationUser>()
-            .AddCms()
-            .AddAdminUserRegistration()
-            .AddEmbeddedLocalization<Startup>();
+        //services
+        //    .AddCmsAspNetIdentity<ApplicationUser>()
+        //    .AddCms()
+        //    .AddEmbeddedLocalization<Startup>();
+		services.AddCmsHost().AddCmsHtmlHelpers().AddCmsUI().AddAdmin().AddVisitorGroupsUI().AddTinyMce();
 
-        services
-        .AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = "azure-cookie";
-            options.DefaultChallengeScheme = "azure";
-        })
-        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-        {
-            options.Events.OnSignedIn = async ctx =>
-            {
-                if (ctx.Principal?.Identity is ClaimsIdentity claimsIdentity)
-                {
-                    // Syncs user and roles so they are available to the CMS
-                    var synchronizingUserService = ctx
-                        .HttpContext
-                        .RequestServices
-                        .GetRequiredService<ISynchronizingUserService>();
+		services.AddAuthentication(options =>
+		{
+			options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+		})
+	    .AddCookie()
+	    .AddOpenIdConnect(options =>
+	    {
+		    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			options.ClientId = "80b9830b-4285-4427-9c4b-6f4e0fcfa5a1";
+			options.Authority = "https://login.microsoftonline.com/" + "e7c989d7-a43c-4fff-9418-a487ec311ffd" + "/v2.0";
+		    options.CallbackPath = "/signin-oidc";
+		    options.Scope.Add("email");
 
-                    await synchronizingUserService.SynchronizeAsync(claimsIdentity);
-                }
-            };
-        })
-        .AddOpenIdConnect("azure", options =>
-        {
-            options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            options.SignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            options.ResponseType = OpenIdConnectResponseType.Code;
-            options.CallbackPath = "/signin-oidc";
-            options.UsePkce = true;
+		    options.TokenValidationParameters = new TokenValidationParameters
+		    {
+			    ValidateIssuer = false,
+			    RoleClaimType = ClaimTypes.Role,
+			    NameClaimType = "preferred_username"
+			};
 
-            // If Azure AD is register for multi-tenant
-            //options.Authority = "https://login.microsoftonline.com/" + "common" + "/v2.0";
-            options.Authority = "https://login.microsoftonline.com/" + "e7c989d7-a43c-4fff-9418-a487ec311ffd" + "/v2.0";
-            options.ClientId = "05d0ac35-a66d-4d1e-ba3d-a6bd9814a4bd";
-            options.ClientSecret = "lV68Q~bQ4Ms8NTmVtYEwonZULXHA1iSHyjbJJcCm";
+		    options.Events.OnAuthenticationFailed = context =>
+		    {
+			    context.HandleResponse();
+			    context.Response.BodyWriter.WriteAsync(Encoding.ASCII.GetBytes(context.Exception.Message));
+			    return Task.FromResult(0);
+		    };
 
-            options.Scope.Clear();
-            options.Scope.Add(OpenIdConnectScope.OpenId);
-            options.Scope.Add(OpenIdConnectScope.OfflineAccess);
-            options.Scope.Add(OpenIdConnectScope.Email);
-            options.MapInboundClaims = false;
+		    options.Events.OnTokenValidated = (ctx) =>
+		    {
+			    var redirectUri = new Uri(ctx.Properties.RedirectUri, UriKind.RelativeOrAbsolute);
+			    if (redirectUri.IsAbsoluteUri)
+			    {
+				    ctx.Properties.RedirectUri = redirectUri.PathAndQuery;
+			    }
+			    //    
+			    //Sync user and the roles to EPiServer in the background
+			    ServiceLocator.Current.GetInstance<ISynchronizingUserService>().SynchronizeAsync(ctx.Principal.Identity as ClaimsIdentity);
+			    return Task.FromResult(0);
+		    };
+	    });
 
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                RoleClaimType = ClaimTypes.Role,
-                NameClaimType = "preferred_username",
-                ValidateIssuer = false
-            };
+		//services
+  //      .AddAuthentication(options =>
+  //      {
+  //          //options.DefaultAuthenticateScheme = "azure-cookie";
+		//	options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		//	options.DefaultChallengeScheme = "azure";
+  //      })
+  //      .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+  //      {
+  //          options.Events.OnSignedIn = async ctx =>
+  //          {
+  //              if (ctx.Principal?.Identity is ClaimsIdentity claimsIdentity)
+  //              {
+  //                  // Syncs user and roles so they are available to the CMS
+  //                  var synchronizingUserService = ctx
+  //                      .HttpContext
+  //                      .RequestServices
+  //                      .GetRequiredService<ISynchronizingUserService>();
 
-            options.Events.OnRedirectToIdentityProvider = ctx =>
-            {
-                // Prevent redirect loop
-                if (ctx.Response.StatusCode == 401)
-                {
-                    ctx.HandleResponse();
-                }
+  //                  await synchronizingUserService.SynchronizeAsync(claimsIdentity);
+  //              }
+  //          };
+  //      })
+  //      .AddOpenIdConnect("azure", options =>
+  //      {
+  //          options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  //          options.SignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  //          options.ResponseType = OpenIdConnectResponseType.Code;
+  //          options.CallbackPath = "/signin-oidc";
+  //          options.UsePkce = true;
 
-                return Task.CompletedTask;
-            };
+  //          // If Azure AD is register for multi-tenant
+  //          //options.Authority = "https://login.microsoftonline.com/" + "common" + "/v2.0";
+  //          options.Authority = "https://login.microsoftonline.com/" + "e7c989d7-a43c-4fff-9418-a487ec311ffd" + "/v2.0";
+  //          options.ClientId = "05d0ac35-a66d-4d1e-ba3d-a6bd9814a4bd";
+  //          options.ClientSecret = "~3o8Q~ZOwauTDQ-HNPQoTfhBE1lsds4DzYu95aS5";
 
-            options.Events.OnAuthenticationFailed = context =>
-            {
-                context.HandleResponse();
-                context.Response.BodyWriter.WriteAsync(Encoding.ASCII.GetBytes(context.Exception.Message));
-                return Task.CompletedTask;
-            };
-        });
+  //          options.Scope.Clear();
+  //          options.Scope.Add(OpenIdConnectScope.OpenId);
+  //          options.Scope.Add(OpenIdConnectScope.OfflineAccess);
+  //          options.Scope.Add(OpenIdConnectScope.Email);
+  //          options.MapInboundClaims = false;
+
+  //          options.TokenValidationParameters = new TokenValidationParameters
+  //          {
+  //              //RoleClaimType = ClaimTypes.Role,
+		//		//NameClaimType = "preferred_username",
+		//		RoleClaimType = "roles",
+		//		NameClaimType = "name",
+		//		ValidateIssuer = false
+  //          };
+
+  //          options.Events.OnRedirectToIdentityProvider = ctx =>
+  //          {
+  //              // Prevent redirect loop
+  //              if (ctx.Response.StatusCode == 401)
+  //              {
+  //                  ctx.HandleResponse();
+  //              }
+
+  //              return Task.CompletedTask;
+  //          };
+
+  //          options.Events.OnAuthenticationFailed = context =>
+  //          {
+  //              context.HandleResponse();
+  //              context.Response.BodyWriter.WriteAsync(Encoding.ASCII.GetBytes(context.Exception.Message));
+  //              return Task.CompletedTask;
+  //          };
+  //      });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
